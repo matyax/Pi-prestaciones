cb9ca05d06f7237d5a179d6d23b63cbb5676197c
Matias Chomicki <matyax@gmail.com>
2014-06-25 17:37:12 -0300

    Add initial menu to event home.

--- Resources/iphone/alloy/controllers/event.js
+++ Resources/iphone/alloy/controllers/event.js
@@ -1,4 +1,13 @@
 function Controller() {
+    function addEventMenuItem(item) {
+        var button = Titanium.UI.createButton({
+            title: item.label,
+            top: "0dp",
+            width: "100%"
+        });
+        button.addEventListener("click", item.onClick);
+        $.eventButtonsView.add(button);
+    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "event";
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
@@ -10,9 +19,17 @@ function Controller() {
         id: "eventWindow"
     });
     $.__views.eventView = Ti.UI.createView({
+        height: Ti.UI.SIZE,
+        top: "0dp",
         id: "eventView"
     });
     $.__views.eventWindow.add($.__views.eventView);
+    $.__views.eventButtonsView = Ti.UI.createView({
+        height: Ti.UI.SIZE,
+        top: "0dp",
+        id: "eventButtonsView"
+    });
+    $.__views.eventWindow.add($.__views.eventButtonsView);
     $.__views.event = Ti.UI.iOS.createNavigationWindow({
         window: $.__views.eventWindow,
         id: "event"
@@ -22,18 +39,31 @@ function Controller() {
     _.extend($, $.__views);
     arguments[0] || {};
     var piApi = require("pi");
+    var eventData = null;
     piApi.getEventDetail(function(event) {
         if (!event) {
             $.event.close();
             return;
         }
+        eventData = event;
         $.eventWindow.setTitle(event.title);
+        event.background_color && $.eventWindow.setBackgroundColor(event.background_color);
         var image = Ti.UI.createImageView({
             image: event.logo,
             width: "100%",
             top: "0dp"
         });
         $.eventView.add(image);
+        var label = "";
+        if (event.information) {
+            label = event.information_label || "Presentación";
+            addEventMenuItem({
+                label: label,
+                onClick: function() {
+                    alert("Clicked");
+                }
+            });
+        }
     });
     _.extend($, exports);
 }
--- app/controllers/event.js
+++ app/controllers/event.js
@@ -2,6 +2,8 @@ var args = arguments[0] || {};
 
 var piApi = require('pi');
 
+var eventData = null;
+
 piApi.getEventDetail(function (event) {
     if (! event) {
         $.event.close();
@@ -9,13 +11,44 @@ piApi.getEventDetail(function (event) {
         return;
     }
     
-   $.eventWindow.setTitle(event.title);
+    eventData = event;
+    
+    $.eventWindow.setTitle(event.title);
+    
+    if (event.background_color) {
+        $.eventWindow.setBackgroundColor(event.background_color);
+    }
    
-   var image = Ti.UI.createImageView({
-      image: event.logo,
-      width: '100%',
-      top: '0dp'
+    var image = Ti.UI.createImageView({
+       image: event.logo,
+       width: '100%',
+       top: '0dp'
     });
     
-    $.eventView.add(image); 
+    $.eventView.add(image);
+    
+    var label = '';
+    
+    if (event.information) {
+        label = event.information_label || 'Presentación';
+        
+        addEventMenuItem({
+            label: label,
+            onClick: function () {
+                alert('Clicked');
+            }            
+        });
+    }
 });
+
+function addEventMenuItem(item) {
+    var button = Titanium.UI.createButton({
+        title: item.label,
+        top: '0dp',
+        width: '100%'       
+    });
+    
+    button.addEventListener('click', item.onClick);
+    
+    $.eventButtonsView.add(button);
+}
