6776250c234b7e9869245fcabbc534be7ae2c3e6
Matias Chomicki <matyax@gmail.com>
2014-08-31 19:43:48 -0300

    Implement initial version image caching.

---
+++ app/lib/cachedImage.js
@@ -0,0 +1,40 @@
+exports.load = function(imageUrl, callback, tempContainer) {
+    var image = null;
+
+    function doRemote(imageUrl) {
+        
+        var file = Ti.Filesystem.getFile('cache/' + localFileName(imageUrl));
+
+        if(! file.exists()) {
+            image = Ti.UI.createImageView({
+                visible: false
+            });
+            
+            image.addEventListener("load", saveImageOnLoad);
+            
+            image.setImage(imageUrl);
+            
+            tempContainer.add(image);
+        } else {
+           callback(file.nativePath);
+        }
+    };
+
+    function saveImageOnLoad(e) {
+        //var file = Ti.Filesystem.getFile('cache/' + localFileName(imageUrl));
+        
+        //file.write(e.source.toBlob());
+        
+        tempContainer.remove(image);
+        
+        callback(imageUrl);
+    };
+
+    function localFileName(imageUrl) {
+        hashedSource = Ti.Utils.md5HexDigest(imageUrl + '');
+        
+        return hashedSource;
+    };
+    
+    doRemote(imageUrl);    
+};
