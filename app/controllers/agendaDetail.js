a5a5aecc133eb5673a277b80db966519fe96f17f
Matias Chomicki <matyax@gmail.com>
2014-09-07 21:58:53 -0300

    Correct typo.

--- app/controllers/agendaDetail.js
+++ app/controllers/agendaDetail.js
@@ -94,7 +94,7 @@ function createAgendaShareView(item) {
     
     var favoriteLabel = Titanium.UI.createLabel({
         text: 'Favoritos',
-        color: eventData.style.share_foreground,        
+        color: eventData.styles.share_foreground,        
         font: {
             fontSize: 12
         },
@@ -112,7 +112,7 @@ function createAgendaShareView(item) {
     
     var tweetLabel = Titanium.UI.createLabel({
         text: 'Twittear',
-        color: eventData.style.share_foreground,
+        color: eventData.styles.share_foreground,
         font: {
             fontSize: 12
         },
