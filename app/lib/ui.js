ada11427fef2ade476e5d2acc65b8b2ebcfe67d8
Matias Chomicki <matyax@gmail.com>
2014-09-23 19:52:40 -0300

    Increase font size.

--- app/lib/ui.js
+++ app/lib/ui.js
@@ -2,17 +2,17 @@ exports.createSectionView = function (eventData, title) {
     var sectionView = Ti.UI.createView({
         backgroundColor: eventData.styles.button_background,
         width: '100%',
-        height: 30,
+        height: 40,
         top: 0,
         left: 0
     });
     
     var sectionLabel = Ti.UI.createLabel({
         color: eventData.styles.button_foreground,
-        font: { fontSize: 15 },
+        font: { fontSize: 16 },
         text: title,
         textAlign: 'left',
-        top: 5,
+        top: 10,
         left: 10,
     });
     
