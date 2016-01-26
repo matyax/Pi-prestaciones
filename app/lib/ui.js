e833b2a21366cfd44205192042cf1d209f266192
Matias Chomicki <matyax@gmail.com>
2016-01-26 15:55:53 -0300

    Clean up ui.js

--- app/lib/ui.js
+++ app/lib/ui.js
@@ -6,7 +6,7 @@ exports.createSectionView = function (eventData, title) {
         top: 0,
         left: 0
     });
-    
+
     var sectionLabel = Ti.UI.createLabel({
         color: eventData.styles.button_foreground,
         font: { fontSize: 16 },
@@ -15,9 +15,9 @@ exports.createSectionView = function (eventData, title) {
         top: 10,
         left: 10,
     });
-    
+
     sectionView.add(sectionLabel);
-    
+
     return sectionView;
 };
 
@@ -31,7 +31,7 @@ exports.screenWidth = function () {
 	if (Titanium.Platform.osname == 'android') {
 	    return pxToDP(Ti.Platform.displayCaps.platformWidth);
 	}
-	
+
 	return Ti.Platform.displayCaps.platformWidth;
 };
 
@@ -39,58 +39,58 @@ exports.screenHeight = function () {
 	if (Titanium.Platform.osname == 'android') {
 	    return pxToDP(Ti.Platform.displayCaps.platformHeight);
 	}
-	
+
 	return Ti.Platform.displayCaps.platformHeight;
 };
 
 exports.processItemConfig = function(item, eventData) {
     if (item.controller == 'news') {
         var unreadNews = getUnreadNewsQuantity(eventData.news);
-        
+
         if (eventData.news.length == 0) {
             item.label += ' (0)';
-            
+
             item.controller == null;
             item.onClick = function () {};
         } else if (unreadNews > 0) {
             item.label += ' (' + unreadNews + ')';
         }
     }
-    
+
     return item;
 };
 
 exports.setLastUnreadNews = function(id) {
     var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, "latest_read_news.dat");
-    
+
     file.write("" + id + "");
-    
+
     return true;
 };
 
 function getUnreadNewsQuantity(news) {
     var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, "latest_read_news.dat");
-    
+
     var blob            = file.read(),
         latestNewsID    = '';
-    
+
     if (blob) {
         latestNewsID = blob.text;
     } else {
         file.write('0');
         latestNewsID = '0';
     }
-    
+
     var unreadNews = 0;
-    
+
     for (var i in news) {
         if (news[i].id == latestNewsID) {
             break;
         }
-        
+
         unreadNews++;
     }
-    
-    
+
+
     return unreadNews;
 }
